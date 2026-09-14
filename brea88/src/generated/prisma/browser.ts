7bec0864913f81c3205079eec6cda44ddac7d634
cylexnoe <131163661+cylexnoe@users.noreply.github.com>
2026-09-14 12:46:39 +0800

    Update browser.ts

--- brea88/src/generated/prisma/browser.ts
+++ brea88/src/generated/prisma/browser.ts
@@ -32,3 +32,8 @@ export type Property = Prisma.PropertyModel
  * 
  */
 export type Inquiry = Prisma.InquiryModel
+/**
+ * Model PushSubscription
+ * 
+ */
+export type PushSubscription = Prisma.PushSubscriptionModel
